150272b51cea44b00f91952a3492c6bd3b160ca9
Tobias Maxham <tobias.maxham@tricept.de>
2015-08-13 01:06:44 +0200

    new with Output

--- Shortener.php
+++ Shortener.php
@@ -7,6 +7,7 @@ class Shortener
    
    private $url = [];
    private $format;
+   private $response;
 
    public function _construct($url = NULL, $format = 'json')
    {
@@ -25,5 +26,19 @@ class Shortener
    public function format($format)
    {
       $this->format = $format;
+   }
+
+   public function out($format = NULL)
+   {
+      if(!is_null($format)) $this->format($format);
+      if(is_null($this->response)) $this->call();
+      return $this->parseResponse();
+   }
+
+   private function parseResponse()
+   {
+      if($this->format == 'plain') return $this->response;
+      $data = json_encode($this->response);
+      return new Responder($data);
    }
 }
